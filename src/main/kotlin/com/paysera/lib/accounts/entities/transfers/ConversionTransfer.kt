564fd35aeb8adaf302dc46a649f55e6000a28a78
Aurimas Žarskis <a.zarskis@paysera.lt>
2020-01-17 17:21:21 +0200

    Fixes entities to match actual response

--- src/main/kotlin/com/paysera/lib/accounts/entities/transfers/ConversionTransfer.kt
+++ src/main/kotlin/com/paysera/lib/accounts/entities/transfers/ConversionTransfer.kt
@@ -4,14 +4,16 @@ import org.joda.money.Money
 import java.util.*
 
 data class ConversionTransfer(
-    val id: Int,
+    val id: String,
+    val identifier: Int,
     val status: String,
     val details: String,
     val accountNumber: String,
-    val clientId: Int,
+    val payer: TransferPayer,
     val from: Money,
     val to: Money,
     val type: String,
     val createdAt: Date?,
-    val performAt: Date?
+    val performAt: Date?,
+    val allowedToCancel: Boolean
 )
