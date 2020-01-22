7a6d2a0e022dafe70c41fec3c0d6e3ddef31fcd0
luksiv <l.sivickas@gmail.com>
2020-01-22 09:01:09 +0200

    Refactor test classes

--- src/test/kotlin/com/paysera/lib/accounts/clients/BaseTest.kt
+++ src/test/kotlin/com/paysera/lib/accounts/clients/BaseTest.kt
@@ -17,7 +17,7 @@ open class BaseTest {
     companion object {
         private val userAgent = "okhttp/3.12.1"
         private val apiCredentials = ApiCredentials(
-            "eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJhdWQiOiJldnBiYW5rIiwiaXNzIjoiYXV0aF9hcGkiLCJleHAiOjE1Nzk3MTM3MjIsImp0aSI6Il9wTzFGVktLS2lRZUdHZ0o4OFZXR1ZwQWhxWV9pWUdqIiwicHNyOnMiOlsibG9nZ2VkX2luIiwiY29uZmlybWVkX2xvZ19pbiJdLCJwc3I6dSI6IjE4NjYwMTIiLCJwc3I6c2lkIjoieEQzcldrUHVSTWNjZzl5RHh4dklTUzlYamRNUUE0aEciLCJwc3I6YSI6eyJ1c2VyX2lkIjoiMTg2NjAxMiJ9LCJpYXQiOjE1Nzk2NzA1MjJ9.vt_s1I3-8hwtAuMg6B_mfGAwkJgjquNNYUy9N79zgqXAEImJIRhRR74t6w-xm_owQAZCfHnktxclfsriCsPCP8Y_dnmgkbsVY97KB_qGbSwne7RHSm-JDID-zU6JQzMzArF59rBfyzH5koOsAMiM4Ttc-AoVwDg38nQi_fibY_XG7AAWKaGvphLlnvw3IPEZfwi2C9yxwV4cSMsMTh_HHpQVgQWtO5_-UE8ZSLYtE3EkarAqJadNYweIqbkz65ua123RfxTBX2VUS9QXfRRNVzm-YVo1QZ3vshKltt25IUGnHnpKIi8ltwW-S1m7QUY4LlKV3arsxkuhj2q3j3aJe7FU6as-KDpC03ZoYRptDaKbxcQVWSTuAmnM53JZ1_rozxuNDzr6AtGRtF14g-BZs7RbLbACcyHPwb-fW-PCQGQoNDKNpaGJVxunnB7eSrSvqpPw0Y1MgRE-DmeC6mboe8SbDfqhLJpY4HSurEodsbCHzIQkpXvvtmTr1Ts4M83G5QuGaNQdjrP4x50YzXwD8CwzXtdPYmvs1goO0C6IX5OoPSkxRxzkOetIEuV_wFvDP9m659DSHzZsqEzhWJsf6acdog-JZoFe-CdP_rmyPa9M4xJT1mqveltfaQkbf7Yfu4lSIQ1shLzpS9LEeMitrmYqIOCzZ6gNNbodVassifE"
+            "your_token"
         )
         private val timeout: Long? = null
         private val loggingLevel = HttpLoggingInterceptor.Level.BODY
