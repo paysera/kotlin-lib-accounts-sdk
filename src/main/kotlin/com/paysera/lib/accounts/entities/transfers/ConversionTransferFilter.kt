8101c0ad7c2fc13af522a0027ba973ca9aed0379
Aurimas Žarskis <a.zarskis@paysera.lt>
2020-01-30 09:53:52 +0200

    Fixes

--- src/main/kotlin/com/paysera/lib/accounts/entities/transfers/ConversionTransferFilter.kt
+++ src/main/kotlin/com/paysera/lib/accounts/entities/transfers/ConversionTransferFilter.kt
@@ -1,6 +1,6 @@
 package com.paysera.lib.accounts.entities.transfers
 
-import com.paysera.lib.accounts.entities.common.BaseFilter
+import com.paysera.lib.common.entities.BaseFilter
 
 data class ConversionTransferFilter(
     val accountNumberList: List<String>,
