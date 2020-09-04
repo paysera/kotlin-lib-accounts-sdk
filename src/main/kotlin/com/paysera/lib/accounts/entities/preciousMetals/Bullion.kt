a7fde8f97900d560651ca36320f3b8fcdfbc1cb3
Aurimas Žarskis <a.zarskis@paysera.lt>
2020-09-04 12:41:59 +0300

    Added hash to Bullion.kt

--- src/main/kotlin/com/paysera/lib/accounts/entities/preciousMetals/Bullion.kt
+++ src/main/kotlin/com/paysera/lib/accounts/entities/preciousMetals/Bullion.kt
@@ -3,6 +3,7 @@ package com.paysera.lib.accounts.entities.preciousMetals
 import org.joda.money.Money
 
 data class Bullion(
+    val hash: String,
     val type: String,
     val identifier: String,
     val amount: Money
