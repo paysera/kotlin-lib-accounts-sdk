1911a4d89a3cecf9649b51acd7459a825b736d3a
Stanislav Georgiev <stanislav.georgiev@paysera.net>
2022-07-03 19:02:02 +0300

    Set default values as nullable

--- src/main/kotlin/com/paysera/lib/accounts/entities/QuestionnaireConfiguration.kt
+++ src/main/kotlin/com/paysera/lib/accounts/entities/QuestionnaireConfiguration.kt
@@ -2,6 +2,6 @@ package com.paysera.lib.accounts.entities
 
 data class QuestionnaireConfiguration(
     val isInitial: Boolean,
-    val featureFlags: List<String>,
-    val areasOfActivity: List<QuestionnaireAreaOfActivity>,
+    val featureFlags: List<String>?,
+    val areasOfActivity: List<QuestionnaireAreaOfActivity>?
 )
